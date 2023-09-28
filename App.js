1c11a4b322975353627ae1b8904628dc0eb78c0b
Marja Nykänen <marja.nykanen@gofore.com>
2023-09-28 12:49:46 +0300

    lisätty SQL + muuta

--- App.js
+++ App.js
@@ -1,60 +1,102 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('shoppingList.db')
 
 export default function App() {
-  const [ text, setText] = useState('');
-  const [data, setData] = useState('');
 
-  const buttonLisaa = () => {
-    setData([...data, { key:text }]);
-    setText('');
-  }
+  const [ title, setTitle] = useState('');
+  const [ amount, setAmount] = useState('');
+  const [shoppingList, setShoppingList] = useState('');
 
-  const buttonTyhjenna = () => {
-    setData([...'']);
-    setText('');
-  }
+  useEffect(() => {
+    db.transaction(tx => {
+    tx.executeSql('create table if not exists shoppingList (id integer primary key not null, title text, amount text);');
+    }, 
+      null,
+      updateList);
+    }, []);  
+
+    const updateList = () => {
+      db.transaction(tx => {
+      tx.executeSql('select * from shoppingList;', [], (_, { rows }) =>
+      setShoppingList(rows._array)
+      );
+      }, null, null);
+      };
+
+      const saveItem = () => {
+        db.transaction(tx => {
+        tx.executeSql('insert into shoppingList (title, amount) values (?, ?);',
+        [title, amount]);
+        }, null, updateList)
+        };
+
+  // const buttonLisaa = () => {
+  //   setData([...data, { key:title, amount }]);
+  //   setTitle('');
+  // }
+
+    
+    const deleteItem = (id) => {
+      db.transaction(
+        tx => tx.executeSql('delete from shoppingList where id = ?;', [id]),
+        null,
+        updateList)
+      };
+
+  // const buttonTyhjenna = () => {
+  //   setData([...'']);
+  //   setTitle('');
+  //   setAmount('');
+  // }
+
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text} />
+      <TextInput style={styles.input} placeholder='Product' onChangeText={title => setTitle(title)} value={title} />
+      <TextInput style={styles.input} placeholder='Amount' onChangeText={amount => setAmount(amount)} value={amount} />
       <View style={styles.operators}>
-      <Button onPress={buttonLisaa} title='Lisää' />
-      <Button onPress={buttonTyhjenna} title='Tyhjennä' />
+      <Button onPress={saveItem} title='Save' />
       </View>
-      <Text style={styles.header}>Ostoslista</Text>
-      <FlatList style={styles.list}
-      data={data}
+      <Text style={styles.header}>Shopping list</Text>
+      <FlatList 
+      style={styles.list}
+      keyExtractor={item => item.id}
       renderItem={({ item }) =>
-
-    <Text> {item.key}</Text>
-      }
-      />
-      <StatusBar style="auto" />
+      <View style={styles.list}>
+        <Text>{item.title}, {item.amount}   </Text>
+        <Text style={{color: 'blue'}} onPress={() => deleteItem(item.id)}>Bought</Text>
+        </View>}
+      data={shoppingList}
+       />
     </View>
   );
-}
+      }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  operators: {
-    width: '50%',
-    marginTop: 50,
+  list: {
+    marginLeft: 50,
+    width: '100%',
+    marginTop: 5,
     flexDirection: 'row',
-    justifyContent: 'space-evenly'
   },
   header: {
     marginTop: 10,
     fontWeight: 'bold',
+    fontSize: 30,
     color: 'blue',
   },
   input: {
-    marginTop: 50,
+    marginTop: 5,
     marginBottom: 5,
     width: 200,
     borderColor: 'gray',
